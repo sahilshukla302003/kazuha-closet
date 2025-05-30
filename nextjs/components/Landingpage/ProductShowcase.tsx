288839e67e77c0c2a4e9fc4ade3b6accbe089339
sahilshukla302003 <148991684+sahilshukla302003@users.noreply.github.com>
2025-05-30 22:44:38 +0530

    build is ok now

--- nextjs/components/Landingpage/ProductShowcase.tsx
+++ nextjs/components/Landingpage/ProductShowcase.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { X, ChevronLeft, ChevronRight } from "lucide-react";
+import {  ChevronLeft, ChevronRight } from "lucide-react";
 import Link from 'next/link';
 
 type Product = {
