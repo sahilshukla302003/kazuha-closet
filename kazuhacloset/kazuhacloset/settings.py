cd101843e4c2adf8414a5f395643a79d19861d65
sahilshukla302003 <148991684+sahilshukla302003@users.noreply.github.com>
2025-08-09 19:09:59 +0530

    backend deployment act 2

--- kazuhacloset/kazuhacloset/settings.py
+++ kazuhacloset/kazuhacloset/settings.py
@@ -80,6 +80,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
